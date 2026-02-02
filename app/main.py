c4452689c024e10d8e74807e24a4a639f1ff60ce
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2026-02-02 18:52:06 +0100

    fixed flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 import os
 
+
 class CleanUpFile:
     # write your code here
-    def __init__(self, filename):
+    def __init__(self, filename: str) -> None:
         self.filename = filename
         self.file = None
 
-    def __enter__(self):
-        self.file = open(self.filename, 'w')
+    def __enter__(self) -> object:
+        self.file = open(self.filename, "w")
         return self.file
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: str, exc_val: int, exc_tb: str) -> None:
         self.file.close()
         os.remove(self.filename)
